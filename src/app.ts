97fbd8612ab043a62ac5b2a766de11008ddc212c
ido110ido <ido110ido@gmail.com>
2023-03-19 16:13:54 +0200

    last time

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import cors from "cors";
 import router from "./index";
 import { connectToDB } from "./connection";
 require("dotenv").config();
+
 const app = express();
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(router);
+
 app.listen(process.env.PORT, () =>
-  console.log(`Listening on http://localhost:8000`)
+  console.log(`Listening on http://localhost:${process.env.PORT}`)
 );
+
 connectToDB();
